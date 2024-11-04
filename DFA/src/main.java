dcb450bcd217e7696e3e36f151475e565ed2a2d0
ahmdWard <ahmdemadabdallah@gmail.com>
2024-11-05 01:29:56 +0200

    Ward Editing

---
+++ DFA/src/main.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class main {
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+         // shof dynamic path ezay grbt bs fslt elsara7a aw n5yrha 3 7sb elghaz ele hy run 
+         
+         // el file da shl msh m7tag shr7
+         
+        String formulaFileName = "/home/ahmed-abdallah/x/computational/formula file.txt";     
+        String tableFileName = "/home/ahmed-abdallah/x/computational/Table File.txt"; 
+
+        try {
+            dfa dfaInstance = new dfa(formulaFileName,tableFileName);
+
+            while (true) {
+                System.out.println("Enter a string to test (or type 'exit' to quit): ");
+                String input = scanner.nextLine();
+
+                if ("exit".equalsIgnoreCase(input)) {
+                    System.out.println("Exiting...");
+                    break;
+                }
+
+                boolean isAccepted = dfaInstance.process(input);
+                if (isAccepted) {
+                    System.out.println("The input string \"" + input + "\" is accepted by the DFA.");
+                } else {
+                    System.out.println("The input string \"" + input + "\" is rejected by the DFA.");
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading DFA configuration files: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+}
